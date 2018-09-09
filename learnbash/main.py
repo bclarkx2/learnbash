952e12cc2340a48dd04e18c5ab61205dc7fcc61d
Brian Clark <bclarkx2@gmail.com>
2018-09-08 21:58:21 -0500

    Added argparse handler to main.py to allow cmd line shortcuts to directly open a specifc learner, course, and lesson. After the completion of the specified unit, the next most general entity will start. i.e. after the course you specified completes, the learner you specified will enter its interactive state.

--- learnbash/main.py
+++ learnbash/main.py
@@ -4,7 +4,35 @@
 # Imports                                                                     #
 ###############################################################################
 
-from learner import Learner
+import argparse
+import importlib
+
+
+###############################################################################
+# CLI                                                                         #
+###############################################################################
+
+def check_learner(learner_name):
+    return check_entity("learner", learner_name)
+
+
+def check_entity(module_name, class_name):
+    _module = importlib.import_module(module_name)
+    _class = getattr(_module, class_name, None)
+    return _class()
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--learner",
+                        default="Learner",
+                        type=check_learner,
+                        help="Learner to start")
+    parser.add_argument("-c", "--course",
+                        help="Name of course to start")
+    parser.add_argument("-l", "--lesson",
+                        help="Name of lesson to start")
+    return parser.parse_args()
 
 
 ###############################################################################
@@ -12,7 +40,21 @@ from learner import Learner
 ###############################################################################
 
 def main():
-    learner = Learner()
+
+    args = get_args()
+
+    learner = args.learner
+
+    if args.course:
+        course_cls = learner.courses.by_name(args.course)
+        if course_cls:
+            course = course_cls()
+            if args.lesson:
+                lesson_cls = course.lessons.by_name(args.lesson)
+                if lesson_cls:
+                    lesson = lesson_cls()
+                    lesson.start()
+            course.start()
     learner.learn()
 
 
